d9670cd7fefe1e17b513e270e2eccb6b604277c4
Remy Bos <27890746+sjokkateer@users.noreply.github.com>
2022-04-02 15:59:35 +0200

    [Fix] PHPCS: App.php

--- src/Core/App.php
+++ src/Core/App.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Core;
 
 use Laminas\Diactoros\ServerRequestFactory;
@@ -24,6 +26,6 @@ class App
 
         $response = $this->router->dispatch($request);
 
-        (new SapiEmitter)->emit($response);
+        (new SapiEmitter())->emit($response);
     }
 }
